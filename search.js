752ef725c2ffd77380ef97ef09c1fadeea1d969e
Platy Hsu <serv@cyhsu.xyz>
2023-10-03 15:27:33 +0800

    prettier

--- search.js
+++ search.js
@@ -1,27 +1,30 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const userBangs = require('./user.json');
+const userBangs = require("./user.json");
 
-app.use(express.static('.'));
+app.use(express.static("."));
 
-app.get('/search', (req, res) => {
-  if (req.method === 'GET') {
+app.get("/search", (req, res) => {
+  if (req.method === "GET") {
     const query = req.query.q;
     let bangMatch = query.match(/^([Vv][0-9A-z]+)\b|\b([Vv][0-9A-z]+)$/g);
     let bang, mainQuery;
 
     if (bangMatch && bangMatch.length >= 1) {
       bang = bangMatch[0].substring(1);
-      mainQuery = query.replace(bangMatch[0], '').trim();
+      mainQuery = query.replace(bangMatch[0], "").trim();
     } else {
       bang = null;
       mainQuery = query.trim();
     }
 
     if (bang) {
-      const bangMatch = userBangs.find(entry => entry.t === bang);
+      const bangMatch = userBangs.find((entry) => entry.t === bang);
       if (bangMatch) {
-        const redirectUrl = bangMatch.u.replace('{{{s}}}', encodeURIComponent(mainQuery));
+        const redirectUrl = bangMatch.u.replace(
+          "{{{s}}}",
+          encodeURIComponent(mainQuery)
+        );
         res.redirect(redirectUrl);
       } else {
         res.redirect(303, `https://duckduckgo.com/?q=!${bang}%20${encodeURIComponent(mainQuery)}`);
@@ -30,10 +33,10 @@ app.get('/search', (req, res) => {
       res.redirect(303, `https://duckduckgo.com/?q=!g%20${encodeURIComponent(mainQuery)}`);
     }
   } else {
-    res.status(405).send({ error: 'Only GET requests are allowed' });
+    res.status(405).send({ error: "Only GET requests are allowed" });
   }
 });
 
 app.listen(3000, () => {
-  console.log(`Server listening on port 3000`)
+  console.log(`Server listening on port 3000`);
 });
